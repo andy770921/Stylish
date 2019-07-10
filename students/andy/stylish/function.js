19bf1a7b5b5d35761ebe9920a7a552bc1203aed3
Andy Chou <andy770921@yahoo.com.tw>
2019-07-10 19:08:30 +0800

    finish get JSON function

--- students/andy/stylish/function.js
+++ students/andy/stylish/function.js
@@ -1,13 +1,26 @@
-function ajax(src){
-    let obj = {};
-    var xhr = new XMLHttpRequest();
-      xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status == 200) {
-            obj = JSON.parse(xhttp.responseText);
-        }
-      };
-      xhr.open('GET', src);
-      xhr.send();
-      return obj;
-  }
-  const abc = ajax("https://api.appworks-school.tw/api/1.0/marketing/hots");
+
+function ajax(src, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status == 200) {
+      callback(JSON.parse(xhr.responseText));
+    }
+  };
+  xhr.open('GET', src);
+  xhr.send();
+}
+
+ajax("https://api.appworks-school.tw/api/1.0/marketing/hots", getJSONObject);
+
+function getJSONObject(parsedData) {
+  //console.log(parsedData.data[0].products[0].images[0]);
+  a = parsedData.data[0].products[0].images[0];
+  createImg(a);
+}
+
+function createImg(url) {
+  const body = document.getElementsByTagName('body')[0];
+  const img = document.createElement('img');
+  img.src = url;
+  body.appendChild(img);
+}
