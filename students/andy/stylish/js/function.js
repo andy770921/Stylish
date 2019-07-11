2d07c746af6156294a0654f50ef0fba463cb0693
Andy Chou <andy770921@yahoo.com.tw>
2019-07-11 16:22:40 +0800

    half finish 6-block content

--- students/andy/stylish/js/function.js
+++ students/andy/stylish/js/function.js
@@ -11,12 +11,20 @@ function ajax(src, callback) {
   xhr.send();
 }
 
-ajax("https://api.appworks-school.tw/api/1.0/marketing/hots", getJSONObject);
+ajax("https://api.appworks-school.tw/api/1.0/marketing/hots", setJSONObject);
 
-function getJSONObject(parsedData) {
+function setJSONObject(parsedData) {
   //console.log(parsedData.data[0].products[0].images[0]);
-  a = parsedData.data[0].products[0].images[0];
-  createImg(a);
+  //a = parsedData.data[0].products[0].images[1];
+  //createImg(a);
+  console.log(parsedData);
+  console.log(parsedData.data[0].products[0].main_image);
+  //設定主頁宣傳圖
+  document.getElementsByClassName('container-3')[0].style.backgroundImage = `url(${parsedData.data[0].products[0].main_image})`;
+  //設定產品圖
+  document.getElementsByClassName('img-4x2')[0].src = parsedData.data[0].products[0].images[1];
+  //設定產品名稱
+  document.getElementsByClassName('text-4x1')[0].innerHTML = `${parsedData.data[0].title}`;
 }
 
 function createImg(url) {
--- students/andy/stylish/js/parameter.js
+++ students/andy/stylish/js/parameter.js
@@ -4,3 +4,18 @@ for (let i = 0; i < navBarWords.length; i++) {
     document.getElementsByClassName(`barItem-${i+1}`)[0].textContent = navBarWords[i];
 }
 
+const imgTextContent = {
+    poet1: ['於是', '我也想要給你', '一個那麼美好的自己。', '不朽《與自己和好如初》'], 
+    poet2: ['A', 'B', 'A', 'B']
+};
+for (let i = 0; i < imgTextContent.poet1.length; i++) {
+    if (i<imgTextContent.poet1.length-1){
+    let p = document.getElementsByClassName('imgWords-1')[0]
+    p.innerHTML += `${imgTextContent.poet1[i]}`;
+    p.appendChild(document.createElement("br")); 
+    } else {
+    let pp = document.getElementsByClassName('imgWords-2')[0]
+    pp.appendChild(document.createElement("br")); 
+    pp.innerHTML += `${imgTextContent.poet1[i]}`;
+    }
+}
