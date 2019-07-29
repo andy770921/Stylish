1bcdfb1cf1c28aef9aebc548e2a2deb05bd7eb6b
Andy Chou <andy770921@yahoo.com.tw>
2019-07-29 21:21:53 +0800

    finish profile html

--- students/andy/stylish/js/profile-page.js
+++ students/andy/stylish/js/profile-page.js
@@ -1,8 +1,19 @@
 if (localStorage.getItem('userData') !== `{"prime":"","order":{"list":[]}}` && localStorage.getItem('userData') !== null) {
     userDataList = JSON.parse(localStorage.getItem('userData'));
-  }
+    // 加入產品圖片
+    const img = document.getElementsByClassName("img-3x1")[0];
+    img.src = userDataList.userPictureUrl;
 
-  createAppendText('item-3x2', 'p', userDataList.userName);
-  createAppendText('item-3x2', 'p', userDataList.userEmail);
-  createAppendImg('item-3x1', userDataList.userPictureUrl);
- //createAppendDiv(parentClassName, childElementType, className);
+    // 加入文字
+    const text1 = document.getElementsByClassName('input-user-name')[0]
+    text1.innerHTML = userDataList.userName;
+    const text2 = document.getElementsByClassName('input-user-name')[0]
+    text2.innerHTML = userDataList.userEmail;
+}
+
+//   createAppendText('item-3x2', 'p', userDataList.userName);
+//   createAppendText('item-3x2', 'p', userDataList.userEmail);
+//   createAppendImg('item-3x1', userDataList.userPictureUrl);
+//createAppendDiv(parentClassName, childElementType, className);
+
+
